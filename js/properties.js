852dbe11b5e870eef4e8d5da0e35e4896d2847e6
khrys63 <khrys63@gmail.com>
2018-11-08 14:33:16 +0100

    Color Properties

--- js/properties.js
+++ js/properties.js
@@ -4,3 +4,21 @@ function getITopUrl(){
 function getIsDebugJSONVisible(){
     return false;
 }
+function getOccupiedColor(){
+    return 'rgb(255, 99, 132)';
+}
+function getFreeColor(){
+    return 'rgb(200, 200, 200)';
+}
+function getNetworkColor(){
+    return 'rgb(190, 162, 235)';
+}
+function getServerColor(){
+    return 'rgb(0, 204, 102)';
+}
+function getStorageColor(){
+    return 'rgb(255, 195, 77)';
+}
+function getEnclosureColor(){
+    return 'rgb(102, 153, 255)';
+}
